0f194e30deaec4e30d217c4bb1ce5727ac68c4c6
bridgar <bridgerhahn@gmail.com>
2014-09-23 09:30:10 -0400

    Completed for assignment

---
+++ roll-a-ball/Assets/Scripts/RedBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
